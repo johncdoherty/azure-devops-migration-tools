2492223122a12e8f2e9a9afead5d02df502a8a63
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2018-03-23 23:00:45 +0000

    Added .net core

---
+++ src/VstsStncMigrator.ConsoleCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VstsStncMigrator.ConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
