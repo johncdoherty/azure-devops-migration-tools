d5e045caafedba0167a03136f6d500b94d6769cf
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2018-04-25 10:20:49 +0100

    Updated Version Number

---
+++ src/VstsSyncMigrator.ConsoleX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VstsSyncMigrator.ConsoleX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
